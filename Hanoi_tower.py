a4f5bdb0544e7548530cf6a73a02eabde2b5136d
TranBaDat2607 <132880494+TranBaDat2607@users.noreply.github.com>
2023-11-23 13:48:22 +0700

    Hanoi_tower.py

--- Hanoi_tower.py
+++ Hanoi_tower.py
@@ -1,10 +1,13 @@
 def hanoi_tower_recursion(disk, colum_1, colum_2, colum_3):
+    # We need to move n disks from colum A to colum B 
     if disk == 1:
         print(f"move {disk} from {colum_1} to {colum_2}")
     else:
+        # Move n-1 disk from colum A to colum C
         hanoi_tower_recursion(disk-1, colum_1, colum_3, colum_2)
-        
+        # Move the biggest disk from colum A to colum B
         hanoi_tower_recursion(1, colum_1, colum_2, colum_3)
+        # Move n-1 disks from colum C to B
         hanoi_tower_recursion(disk-1, colum_3, colum_2, colum_1)
 disk = int(input("How many disk: "))
 colum_1 = "A"
